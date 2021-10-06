36d14dddb42a3b305df07d14ef253b0a9bef441c
PawelOzdoba-dev <pawelozdobasoftware@gmail.com>
2021-10-06 18:43:34 +0200

    - ignore files, project initialization

---
+++ src/main/java/pl/ozdoba/posts/PostsAndCommentsApplication.java
@@ -0,0 +1,13 @@
+package pl.ozdoba.posts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsAndCommentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostsAndCommentsApplication.class, args);
+    }
+
+}
